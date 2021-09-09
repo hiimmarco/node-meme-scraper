13db374a7589c421a1d842f41410b28e5d4a3d4e
Marco Bogner <marcobogner@gmail.com>
2021-09-09 17:25:00 +0200

    Add function to get URLs

--- index.js
+++ index.js
@@ -3,14 +3,40 @@
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
+// Fetch the HTML
+
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app',
 );
 const body = await response.text();
 
-// console.log(body);
-
+// Pass HTML to cheerio to get URLs
+/*
 let $ = cheerio.load(body);
-let imageSource = $('img').attr('src');
+let imageSource = $('div img').attr('src');
 
 console.log(imageSource);
+*/
+
+/* Here I tried a function that loops over the URLs, but only get the same first URL 10 times.
+function getTenUrls() {
+  for (let i = 0; i < 10; i++) {
+    let $ = cheerio.load(body);
+    let imageSource = $('div img').attr('src');
+    console.log(imageSource);
+  };
+};
+const tenUrls = getTenUrls();
+*/
+
+let $ = cheerio.load(body);
+
+function getUrls() {
+  $('img').each((i, el) => {
+    const tenUrls = $(el).attr('src');
+    console.log(tenUrls);
+    return tenUrls;
+  });
+}
+
+getUrls();
