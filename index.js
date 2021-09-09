497e809aa9d0898bb54f3b3954ef39767c1399e3
Marco Bogner <marcobogner@gmail.com>
2021-09-09 19:18:20 +0200

    Add image downloader and code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Import image downloader to download image to folder
 
 import cheerio from 'cheerio';
+import download from 'image-downloader';
 import fetch from 'node-fetch';
 
 // Fetch the HTML
@@ -15,6 +16,7 @@ const body = await response.text();
 const $ = cheerio.load(body);
 
 // Declare a new array to save all image URLs from the website
+
 const imageLinks = [];
 
 // Get only the imageURLs and push them into the newly created array "tenLinks"
@@ -29,4 +31,18 @@ $('img').each((i, el) => {
 
 const tenImageLinks = imageLinks.slice(0, 10);
 
-console.log(tenImageLinks);
+// console.log(tenImageLinks);
+
+// Download images to folder
+
+const options = {
+  url: 'https://api.memegen.link/images/keanu.jpg?width=300',
+  dest: './memes', // will be saved to ./memes/image.jpg
+};
+
+download
+  .image(options)
+  .then(({ filename }) => {
+    console.log('Saved to', filename); // saved to ./memes/image.jpg
+  })
+  .catch((err) => console.error(err));
