0c0c9f43baf1d38668751c2a224a1cee374ba242
Marco Bogner <marcobogner@gmail.com>
2021-09-09 15:19:03 +0200

    Add first html fetch

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hallo Mampfred.');
+// Import node-fetch for fetching the html
+
+import fetch from 'node-fetch';
+
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app',
+);
+const body = await response.text();
+
+console.log(body);
